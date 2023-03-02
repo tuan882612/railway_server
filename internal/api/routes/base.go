0d0ef88ec67b3d8f98c982198bc6421945e2e3bd
tuan <tuan882612@gmail.com>
2023-03-02 01:35:55 -0600

    feat: implemented password hashing and server health endpoint

--- internal/api/routes/base.go
+++ internal/api/routes/base.go
@@ -1,7 +1,12 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"main/internal/api/handler"
+
+	"github.com/gin-gonic/gin"
+)
 
 func BaseRoutes(engine *gin.RouterGroup) {
-	engine.GET("/")
+	engine.GET("/", handler.Default)
+	engine.GET("/health", handler.Health)
 }
