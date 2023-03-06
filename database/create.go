fefc2d642081d54bc108fe353a61b29f5a086fb0
tuan882612 <tuan882612@gmail.com>
2023-03-06 16:25:58 -0600

    feat: Completed scripts for creating the tables into cockroachdb and started on the scripts for loading the data into the db.

---
+++ database/create.go
@@ -0,0 +1,62 @@
+package database
+
+import (
+	"context"
+	"log"
+)
+
+func CreateTables() {
+	client := GetClient()
+	tables := []string{`
+		CREATE TABLE Passenger (
+			ssn INTEGER PRIMARY KEY,
+			first_name VARCHAR(50),
+			last_name VARCHAR(50),
+			address VARCHAR(255),
+			city VARCHAR(50),
+			county VARCHAR(50),
+			phone2 VARCHAR(15),
+			bdate DATE
+		);
+	`,`
+		CREATE TABLE Train (
+			train_no INTEGER PRIMARY KEY,
+			name VARCHAR(50),
+			premium_fare INTEGER,
+			general_fare INTEGER,
+			source VARCHAR(50),
+			destination VARCHAR(50),
+			available DATE
+		);	
+	`,`
+		CREATE TABLE Train_Status (
+			train_no integer,
+			date date,
+			ps_available integer,
+			gs_available integer,
+			ps_occupied integer,
+			gs_occupied integer, 
+			FOREIGN KEY (train_no) REFERENCES Train (train_no)
+		);
+	`,`
+		CREATE TABLE Booked (
+			ssn INTEGER,
+			train_no INTEGER,
+			category VARCHAR(10),
+			status VARCHAR(10),
+			FOREIGN KEY (ssn) REFERENCES Passenger (ssn),
+			FOREIGN KEY (train_no) REFERENCES Train (train_no)
+		);
+	`}
+
+	for _, table := range tables {
+		_, err := client.Exec(
+			context.Background(), 
+			table,
+		)
+		if err != nil {
+			log.Fatal("Unable to create table:", err)
+		}
+	}
+
+}
